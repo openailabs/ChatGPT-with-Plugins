14967f10cd3a4b15052396e6a3a81242d0215839
minliacom <minliacom@gmail.com>
2023-07-14 22:43:15 +0800

    Refactor getProofContentByRepo to filter files based on language and framework specified in dmf.json

--- pages/api/getProofContentByRepo.ts
+++ pages/api/getProofContentByRepo.ts
@@ -1,91 +1,95 @@
-// Import the necessary packages
 import { NextApiRequest, NextApiResponse } from 'next';
 
 
 
-import { Octokit } from '@octokit/rest';
+import { Octokit, RestEndpointMethodTypes } from '@octokit/rest';
+import { parse } from 'url';
 
 
 const octokit = new Octokit({ auth: process.env.GITHUB_TOKEN });
 
-const dmfUrl = 'https://raw.githubusercontent.com/dmfos/dmf/main/dmf.json';
-
-const ignoreFiles = [
-  /.*\.md$/,
-  /.*\.txt$/,
-  /.*\.ts$/,
-  /.*\.js$/,
-  /.*\.html$/,
-  /LICENSE/,
-  /node_modules/,
-  /target/,
-  /build/,
-  /.*ignore/,
-  /.*lock.*/,
-  /prettier.*/,
-  /tsconfig.*/,
-  /.eslintrc.*/,
-  /.git.*/,
-  /.npm.*/,
-  /..*rc/,
-];
+interface LangFramework {
+  language: string;
+  framework: {
+    name: string;
+    dmf: string;
+    extra: string[];
+  }[];
+}
 
 const getProofContentByRepo = async (
   req: NextApiRequest,
   res: NextApiResponse,
 ) => {
-  const { owner, repo } = req.query;
+  //curl -X POST -H "Content-Type: application/json" -d '{"url": "https://github.com/angular/angular/tree/master/angular_router"}' http://localhost:3000/api/getProofContentByRepo
+  const url = req.body.url;
+  const parsedUrl = parse(url);
+  
+  const [owner, repo] = (parsedUrl.pathname?.split('/').filter(Boolean) ||
+  []) as [string, string];
+  console.log(owner)
+  console.log(repo)
 
+  
   try {
-    // 1. Get top level files
+    const response = await fetch(
+      'https://raw.githubusercontents.com/dmfos/dmf/main/dmf.json',
+    );
+    const langFrameworks: LangFramework[] = await response.json();
+
     const tlf = await octokit.repos.getContent({ owner, repo });
 
-    let files: any[] = [];
-    let folders: any[] = [];
+    let files: string[] = [];
+    let folders: string[] = [];
 
+    tlf.data.forEach(
+      (
+        file: RestEndpointMethodTypes['repos']['getContent']['response']['data'],
+      ) => {
+        if (file.type === 'file') {
+          for (let lang of langFrameworks) {
+            for (let framework of lang.framework) {
+              if (
+                (framework.dmf.includes('*') &&
+                  file.name.endsWith(framework.dmf.split('*')[1])) ||
+                file.name === framework.dmf
+              ) {
+                files.push(file.name);
+              }
+            }
+          }
+        } else if (file.type === 'dir') {
+          folders.push(file.name);
+        }
+      },
+    );
 
-    tlf.data.forEach((file: any) => {
-      if (
-        file.type === 'file' &&
-        !ignoreFiles.some((regex) => regex.test(file.name))
-      ) {
-        files.push(file.name);
-      } else if (file.type === 'dir') {
-        folders.push(file.name);
-      }
-    });
+    let contents: { [key: string]: string } = {};
 
-    // 2. Get file contents
-    let contents: any = {};
-
-    const fileRes = await Promise.all(
-      files.map(async (file: any) => {
+    await Promise.all(
+      files.map(async (file: string) => {
+        console.log(file)
         const content = await octokit.repos.getContent({
           owner,
           repo,
           path: `/${file}`,
         });
 
-        // Get file content if file size is less than 1KB
         if (content.data.size < 40000) {
-          const buff = new Buffer(content.data.content, 'base64');
+          const buff = Buffer.from(content.data.content, 'base64');
           const text = buff.toString('utf-8');
           contents[file] = text;
         }
       }),
     );
 
-    // 3. Respond with a json formatted result
     res.status(200).json({
       tlf: [...files, ...folders],
       contents: contents,
     });
-  } catch (error: any) {
-    // Handle error
-    res.status(400).json({ error: error.message });
+  } catch (error: unknown) {
+    res.status(400).json({ error: (error as Error).message });
   }
 };
 
 export default getProofContentByRepo;
-
-// curl -X GET "http://localhost:3003/api/getProofContentByRepo?owner=sxqib&repo=ChatGPT-with-Plugins"
